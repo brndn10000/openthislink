fe99e99c4cdb9408b7e07e909cd727f066baabff
brndn10000 <159673007+brndn10000@users.noreply.github.com>
2024-02-10 21:57:15 -0500

    Add files via upload

---
+++ Code/script.js
@@ -0,0 +1,7 @@
+function chooseResponse(response) {
+  if (response === 'yes') {
+    window.location.href = "iloveyou.html";
+  } else if (response === 'no') {
+    alert("Oh, that's okay! Have a great day!");
+  }
+}
